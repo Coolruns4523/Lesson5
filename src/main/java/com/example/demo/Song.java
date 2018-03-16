5a5627b43c68664db23274d03a268fbe9ee36e8a
GBTC440003UR <brandong10@me.com>
2018-03-16 09:21:13 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+public class Song {
+    private long id;
+    private String name;
+    private String album;
+    private int rating;
+    private int year;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
